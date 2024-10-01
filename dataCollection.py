62986bd0a021e5a687ddbe5be11e60cbde9d1638
Andre Fugledal <andre.fugledal@gmail.com>
2024-10-01 13:38:08 +0200

    merge

--- dataCollection.py
+++ dataCollection.py
@@ -1,4 +1,12 @@
 import pandas as pd
+import os
 import matplotlib.pyplot as plt
 
-
+# Load the CSV data
+data = pd.read_csv('Data/0-calibration_fts-accel.csv')
+# Plot the data
+data.plot()
+plt.xlabel('Index')
+plt.ylabel('Values')
+plt.title('Data Plot')
+plt.show()
