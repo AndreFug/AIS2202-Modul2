c6d4382f3896ff459b666ae0e8259e3b8e474be8
jo-halle <josve@stud.ntnu.no>
2024-10-16 14:48:03 +0200

    Changed the variable names to be match the ones in the article An Introduction to the Kalman Filter.

--- KalmanFilter.py
+++ KalmanFilter.py
@@ -1,31 +1,30 @@
 import numpy as np
 
 class KalmanFilter:
-    def __init__(self, F, B, H, Q, R, x0, P0):
+    def __init__(self, A, B, H, Q, R, P, x0):
 
-        # Model variables
-        self.F = F  
-        self.B = B  
-        self.H = H  
-        self.Q = Q  
-        self.R = R  
-        self.x = x0  
-        self.P = P0 
-    
-    def predict(self, u):   # Prediction step (a priori)
+        self.A = A
+        self.B = B
+        self.H = H
+        self.Q = Q
+        self.R = R
+        self.P = P
+        self.x = x0
 
-        self.x = np.dot(self.F, self.x) + np.dot(self.B, u)
-        self.P = np.dot(np.dot(self.F, self.P), self.F.T) + self.Q
+    def predict(self, u=0):
+        # Predict the next state
+        self.x = np.dot(self.A, self.x) + np.dot(self.B, u)
+        # Predict the next covariance
+        self.P = np.dot(np.dot(self.A, self.P), self.A.T) + self.Q
         return self.x
-    
-    def update(self, z):    # Update step (a posteriori)
 
-        S = np.dot(np.dot(self.H, self.P), self.H.T) + self.R
-        K = np.dot(np.dot(self.P, self.H.T), np.linalg.inv(S))      
-        y = z - np.dot(self.H, self.x)  
+    def update(self, z):
+        # Calculate the Kalman Gain
+        S = np.dot(self.H, np.dot(self.P, self.H.T)) + self.R
+        K = np.dot(np.dot(self.P, self.H.T), np.linalg.inv(S))
+        # Update the state estimate
+        y = z - np.dot(self.H, self.x)  # Measurement residual
         self.x = self.x + np.dot(K, y)
-        I = np.eye(self.P.shape[0])
-        self.P = np.dot(np.dot(I - np.dot(K, self.H), self.P), (I - np.dot(K, self.H)).T) + np.dot(np.dot(K, self.R), K.T)
+        # Update the estimate error covariance
+        self.P = np.dot((np.eye(self.P.shape[0]) - np.dot(K, self.H)), self.P)
         return self.x
-    
-
