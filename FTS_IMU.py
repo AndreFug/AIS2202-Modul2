90fa6ba30142330adc96c4f185822f502574c10c
Andre Fugledal <andre.fugledal@gmail.com>
2024-10-17 11:40:02 +0200

    checked right values against "fasit", edited decimal amounts to 15.

--- FTS_IMU.py
+++ FTS_IMU.py
@@ -1,5 +1,7 @@
 import pandas as pd
 
+pd.set_option('display.precision', 15)
+
 imu_file = './Data/0-steady-state_accel.csv'
 fts_file = './Data/0-steady-state_wrench.csv'
 
@@ -9,5 +11,9 @@ fts_data = pd.read_csv(fts_file)
 imu_variances = imu_data.var()
 fts_variances = fts_data.var()
 
+fts_f_xyz_variances = fts_variances[['fx', 'fy', 'fz']]
+fts_t_xyz_variances = fts_variances[['tx', 'ty', 'tz']]
+
 print("IMU Signal Variances:\n",imu_variances)
-print("\nFTS Signal Variances:\n",fts_variances)
+print("\nFTS Force Variances:\n", fts_f_xyz_variances)
+print("\nFTS Torque Variances:\n", fts_t_xyz_variances)
