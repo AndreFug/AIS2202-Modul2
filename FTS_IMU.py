ef4bdd65a28976aba64960abe00ff4ed317aa7ac
Andre Fugledal <andre.fugledal@gmail.com>
2024-10-17 08:07:09 +0200

    removed function

--- FTS_IMU.py
+++ FTS_IMU.py
@@ -1,28 +1,13 @@
 import pandas as pd
 
-def calculate_variance(file_path):
-    # Read the CSV file
-    data = pd.read_csv(file_path)
-    
-    # Calculate the variance for each column
-    variances = data.var()
-    
-    return variances
+imu_file = './Data/0-steady-state_accel.csv'
+fts_file = './Data/0-steady-state_wrench.csv'
 
-def main():
-    # File paths
-    imu_file = './Data/0-steady-state_accel.csv'
-    fts_file = './Data/0-steady-state_wrench.csv'
-    
-    # Calculate variances
-    imu_variances = calculate_variance(imu_file)
-    fts_variances = calculate_variance(fts_file)
-    
-    # Print the results
-    print("IMU Signal Variances:")
-    print(imu_variances)
-    print("\nFTS Signal Variances:")
-    print(fts_variances)
+imu_data = pd.read_csv(imu_file)
+fts_data = pd.read_csv(fts_file)
 
-main()
+imu_variances = imu_data.var()
+fts_variances = fts_data.var()
 
+print("IMU Signal Variances:\n",imu_variances)
+print("\nFTS Signal Variances:\n",fts_variances)
